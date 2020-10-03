7381b16f6a31c990f62158579e9fabb20a07eb2c
Pradyumn Shukla <ps811876@student.nitw.ac.in>
2020-10-03 10:27:53 +0530

    API Done

--- validators/auth.js
+++ validators/auth.js
@@ -1,37 +1,37 @@
 const { check } = require("express-validator");
 
 exports.userRegisterValidator = [
-	check("name").not().isEmpty().withMessage("Name is required"),
+  check("name").not().isEmpty().withMessage("Name is required"),
 
-	check("email").isEmail().withMessage("Email is not valid"),
+  check("email").isEmail().withMessage("Email is not valid"),
 
-	check("password")
-		.isLength({ min: 6 })
-		.withMessage("Password must be at least 6 characters long"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
 
-	check("categories")
-		.isLength({ min: 6 })
-		.withMessage("Pick at least 1 favourite category"),
+  // check("categories")
+  // 	.isLength({ min: 6 })
+  // 	.withMessage("Pick at least 1 favourite category"),
 ];
 
 exports.userLoginValidator = [
-	check("email").isEmail().withMessage("Email is not valid"),
-	check("password")
-		.isLength({ min: 6 })
-		.withMessage("Password must be at least 6 characters long"),
+  check("email").isEmail().withMessage("Email is not valid"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
 ];
 
 exports.userEmailValidator = [
-	check("email").isEmail().withMessage("Email is not valid"),
+  check("email").isEmail().withMessage("Email is not valid"),
 ];
 
 exports.resetPasswordValidator = [
-	check("newPassword")
-		.isLength({ min: 6 })
-		.withMessage("Password must be at least 6 characters long"),
-	check("resetPasswordLink").not().isEmpty().withMessage("Token is Required"),
+  check("newPassword")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  check("resetPasswordLink").not().isEmpty().withMessage("Token is Required"),
 ];
 
 exports.userUpdateValidator = [
-	check("name").not().isEmpty().withMessage("Name is required"),
+  check("name").not().isEmpty().withMessage("Name is required"),
 ];
